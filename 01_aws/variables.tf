56ddc53daffe996c49803aa4e5a36f34bb25d0e6
Elyan <ekenimohelyanpheta@gmail.com>
2025-04-10 09:23:11 -0700

    creating resources

---
+++ 01_aws/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr_block" {
+  description = "cidr block for the Vpc"
+  type = string
+}
+
+variable "Web_Subnet" {
+  description = "web subnet"
+  type = string
+}
+
+variable "subnet_zone" {
+  
+}
+
+variable "main_vpc_name" {
+  
+}
