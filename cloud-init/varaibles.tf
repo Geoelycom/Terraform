38d305615606123d9abbeba50b3174a11f2517a7
Elyan <ekenimohelyanpheta@gmail.com>
2025-04-17 03:26:02 -0700

    update variables

---
+++ cloud-init/varaibles.tf
@@ -0,0 +1,24 @@
+variable "vpc_cidr_block" {
+  description = "cidr block for the Vpc"
+  type = string
+}
+
+variable "Web_Subnet" {
+  description = "web subnet"
+  type = string
+}
+
+variable "subnet_zone" {
+  
+}
+
+variable "main_vpc_name" {
+  
+}
+
+variable "my_public_ip" {
+}
+
+variable "ssh_public_key" {
+  
+}
