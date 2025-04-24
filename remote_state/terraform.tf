5018412b8e6e9852e275e10b499282e41a4cda05
Elyan <ekenimohelyanpheta@gmail.com>
2025-04-24 10:06:10 -0700

    Add auto.tfvars file

--- remote_state/terraform.tf
+++ remote_state/terraform.tf
@@ -16,4 +16,6 @@ terraform {
   }
 
   ## When we are trying to use VSC-DRIVEN workflow for hcp, WE DONT NEED to define the cloud block configuration in our terraform block. hence we commented it out
+
+  ## after connecting and commiting to github, you visit the terraform workspace/settings and click connect to version control. select github and follow onscreen instruction. select github.custom
 }
