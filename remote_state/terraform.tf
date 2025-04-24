b46e7e96a5ccfc351b0c37af955d7a0d9b775cee
Elyan <ekenimohelyanpheta@gmail.com>
2025-04-24 09:45:59 -0700

    Remove cloud block

--- remote_state/terraform.tf
+++ remote_state/terraform.tf
@@ -1,9 +1,19 @@
 terraform {
-  cloud {
-    organization = "Easepay"
+  # cloud {
+  #   organization = "Easepay"
 
-    workspaces {
-      name = "Geo-terraform"
+  #   workspaces {
+  #     name = "Geo-terraform"
+  #   }
+  # }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+    required_version = ">= 1.0.0"
   }
+
+  ## When we are trying to use VSC-DRIVEN workflow for hcp, WE DONT NEED to define the cloud block configuration in our terraform block. hence we commented it out
 }
