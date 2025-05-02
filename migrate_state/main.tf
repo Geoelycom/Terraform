695f3ec42b6ae9f3b70c94f6fba40445c0799eea
Elyan <ekenimohelyanpheta@gmail.com>
2025-05-02 16:37:27 -0700

    migrate state from local to remote cloud hcp

---
+++ migrate_state/main.tf
@@ -0,0 +1,32 @@
+## Terraform configuration for migrating state to Terraform
+
+terraform {
+  cloud {
+    organization = "Easepay"
+    workspaces {
+      name = "migrate_state"
+    }
+  }
+  
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "3"
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "-"
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
